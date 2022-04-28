6bb6d979b7d489f9c0e08a031c240a00c5d12a98
Alex Geis <siegxela@gmail.com>
2022-04-28 09:02:10 -0600

    add proxy to package.json, attempting to fix nav css

--- src/App/components/Nav/Nav.js
+++ src/App/components/Nav/Nav.js
@@ -4,33 +4,26 @@ import "./Nav.css";
 
 export default function Nav() {
   return (
-    <header className="navigation navStyle">
-      <a
-        className="item underline get-started js-scroll-item navItemStyle"
-        href="#work"
-      >
-        Work
-      </a>
-      <a
-        className="item underline whos-using-it js-scroll-item navItemStyle"
-        href="#contact"
-      >
-        Contact
-      </a>
-      <a
-        className="item underline features js-scroll-item navItemStyle"
-        href="#about"
-      >
-        About
-      </a>
-      <a
-        className="item underline github navItemStyle"
-        href="https://github.com/alexgeis/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={GitHubLogo} alt="github logo"></img>
-      </a>
-    </header>
+    <>
+      <header className="navigation navStyle">
+        <a className="item underline navItemStyle" href="#work">
+          Work
+        </a>
+        <a className="item underline navItemStyle" href="#contact">
+          Contact
+        </a>
+        <a className="item underline navItemStyle" href="#about">
+          About
+        </a>
+        <a
+          className="item underline github navItemStyle"
+          href="https://github.com/alexgeis/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={GitHubLogo} alt="github logo"></img>
+        </a>
+      </header>
+    </>
   );
 }
